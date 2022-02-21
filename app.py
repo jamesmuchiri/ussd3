ca03c2b1750cde35956de4bc12112b41c66d7680
the-gamers45 <42207280+the-gamers45@users.noreply.github.com>
2022-02-21 13:23:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from ast import Return
 from flask import Flask, request
 import africastalking
 import os
@@ -9,6 +8,7 @@ from datetime import datetime
 import datetime as dt
 from dateutil.parser import parse
 import mysql.connector
+from menu import Menu
 from flask import make_response
 app = Flask(__name__)
 
@@ -22,62 +22,48 @@ sms = africastalking.SMS
     
 @app.route('/', methods=['POST', 'GET'])
 
-def ussd_proceed(menu_text):
-        #redis.set(self.session_id, json.dumps(self.session))
-    menu_text = "CON {}".format(menu_text)
-    response = make_response(menu_text, 200)
-    response.headers['Content-Type'] = "text/plain"
-    return response
+class Callback(Menu):
 
-def ussd_end(menu_text):
-        #redis.delete(self.session_id)
-    menu_text = "END {}".format(menu_text)
-    response = make_response(menu_text, 200)
-    response.headers['Content-Type'] = "text/plain"
-    return response
-def home():
-    kenya_time = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
-    if 5<= kenya_time <12 :
-        Good_Morning="Good Morning"
-        menu_text =("CON {}"
-                        "\nHow may i help you"
-                        "\n  -Limit "
-                        "\n  -Balance"
-                        "\n  -Loan"
-                        "\n  -Amount"
-            ).format(Good_Morning)
-    elif  12 <= kenya_time < 17 :
-        Good_Afternoon="Good Afternoon"
-        menu_text =("CON {}"
-                        "\nHow may i help you"
-                        "\n  -Limit "
-                        "\n  -Balance"
-                        "\n  -Loan"
-                         "\n  -Amount"
-                    ).format(Good_Afternoon)
-    else:
-        Good_Evening="Good Evening"
-        menu_text =("CON {}"
-                        "\nHow may i help you"
-                        "\n  -Limit "
-                        "\n  -Balance"
-                        "\n  -Loan"
-                        "\n  -Amount"
-                    ).format(Good_Evening)
-    return ussd_proceed(menu_text)
+    def Callback(self):
+        phone_number = request.values.get("phoneNumber","default")
+        text = request.values.get("text","default")
+        text_array = text.split("*")
+        user_response = text_array[len(text_array) - 1]
+            
+    
+        if user_response == "":
+            kenya_time = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
+            if 5<= kenya_time <12 :
+                Good_Morning="Good Morning"
+                menu_text =("CON {}"
+                            "\nHow may i help you"
+                            "\n  -Limit "
+                            "\n  -Balance"
+                            "\n  -Loan"
+                            "\n  -Amount"
+                ).format(Good_Morning)
+            elif  12 <= kenya_time < 17 :
+                Good_Afternoon="Good Afternoon"
+                menu_text =("CON {}"
+                            "\nHow may i help you"
+                            "\n  -Limit "
+                            "\n  -Balance"
+                            "\n  -Loan"
+                            "\n  -Amount"
+                        ).format(Good_Afternoon)
+            else:
+                Good_Evening="Good Evening"
+                menu_text =("CON {}"
+                            "\nHow may i help you"
+                            "\n  -Limit "
+                            "\n  -Balance"
+                            "\n  -Loan"
+                            "\n  -Amount"
+                        ).format(Good_Evening)
 
-def Callback():
-    phone_number = request.values.get("phoneNumber","default")
-    text = request.values.get("text","default")
-    text_array = text.split("*")
-    user_response = text_array[len(text_array) - 1]
         
-        
-
-    if user_response == "":
-        return home()
-        
-    return user_response  
+        return self.ussd_proceed(menu_text)
+  
 
         
 if __name__ == "__main__":
